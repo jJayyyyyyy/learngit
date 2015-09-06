cdbe700010a1bbd6062b08a13f33bba6843883f7
Steve <ljjdbd123@hotmail.com>
2015-09-06 17:16:07 +0800

    add quick_sort_part.cpp

---
+++ quick_sort_part.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+//(left to right) is (0 to n-1)  
+int quick_sort_part(int a[], int left, int right){
+	int i=left, j=right;
+	int key = a[i];
+
+	while(i<j){
+		while(i<j && a[j]>key)
+			j -= 1;
+		if(i<j){
+			a[i] = a[j];
+			i += 1;
+		}
+		while(i<j && a[i]< key)
+			i += 1;
+		if(i<j){
+			a[j] = a[i];
+			j -= 1;
+		}
+	}
+	a[i] = key;
+	
+	return i;
+}
+
+void quick_sort(int a[], int left, int right){
+	if(left < right){
+		int i = quick_sort_part(a, left, right);
+		quick_sort(a, left, i-1);
+		quick_sort(a, i+1, right);
+	}
+}
+
+
+int main(){
+	int a[] = {2, 1, 4, 8, 10, 5, 4, 11, 3};
+	int len = sizeof(a)/sizeof(a[0]);
+
+	quick_sort(a, 1-1, len-1);
+	for(int i=0; i<len; i++){
+		cout<<a[i]<<'\t';
+	}
+	
+	return 1;
+}
+
