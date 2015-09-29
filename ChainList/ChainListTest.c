574505cc97575bdd8f8cff9f5eaa1e92a133bfc9
Steve <ljjdbd123@hotmail.com>
2015-09-29 23:21:26 +0800

    modified ChainListTest.c

--- ChainList/ChainListTest.c
+++ ChainList/ChainListTest.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <string.h>
 #include "ChainList.h"
+
+ChainListType *ChainListInit(ChainListType *head){
+    DATA data;
+    head=NULL;
+    
+    printf("Please input data of List, including key, name and age. ");
+    printf("Quit when key=0.\n");
+    while (1) {
+        fflush(stdin);
+        scanf("%s", data.key);
+        if(strcmp(data.key, "0") == 0)
+            break;
+        
+        scanf("%s%d",data.name, &data.age);
+        head = ChainListAddEnd(head, data);
+    }
+    
+    return head;
+}
+
+void ChainListTraverse(ChainListType *head){
+    ChainListType *p;
+    DATA data;
+    
+    p = head;
+    if (p == NULL) {
+        printf("List is empty!\n");
+    }else{
+        printf("All the data is as follows.\n");
+        while (p != NULL) {
+            data = p->data;
+            printf("(%s, %s, %d)\n", data.key, data.name, data.age);
+            p = p->next;
+        }
+    }
+}
+
