1aa36e53a1263692bfbccab2eb204964d9277af1
Steve <ljjdbd123@hotmail.com>
2015-10-01 22:17:01 +0800

    add test ChainListFind() in main.c

--- ChainList/main.c
+++ ChainList/main.c
@@ -10,9 +10,17 @@ int main(){
     head = ChainListInit();
     ChainListTraverse(head);
     
+    ///Find out if the is a Node that matches the key
+    printf("\n");
+    printf("Please input the key of Node you want to find: ");
+    scanf("%s", key);
+    ChainListFind(head, key);
+    
+    
     //Insert a Node
     //Use key to find where to insert the Node
     //Then input the Data of that Node
+    printf("\n");
     printf("\nInput key to insert a Node:");
     scanf("%s",key);
     if(ChainListFind(head, key)) {
