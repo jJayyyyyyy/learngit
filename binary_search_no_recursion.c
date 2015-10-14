60902c829007e2a52edc462cb0a106bb70426186
Steve <ljjdbd123@hotmail.com>
2015-10-14 20:28:15 +0800

    declare [int m] in while-loop instaed of outside.

---
+++ binary_search_no_recursion.c
@@ -0,0 +1,15 @@
+int BinSearch(int a[],int len, int key)
+{
+    int left = 0,right = len-1;
+    while (left <= right)
+    {
+        int mid = (left + right)/2;
+        if(key == a[mid])
+            return mid;
+        if(key < a[mid])
+            right = mid-1;
+        if(key > a[mid])
+            left = mid+1;
+    }
+    return -1;
+}
