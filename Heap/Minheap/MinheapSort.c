2206297303ab6f4fbfba04fb0a7c619fe3cddb29
Steve <ljjdbd123@hotmail.com>
2015-11-01 16:15:54 +0800

    modified FixUp() & AddNumber(), add FixDown() & DeleteNumber()

--- Heap/Minheap/MinheapSort.c
+++ Heap/Minheap/MinheapSort.c
@@ -4,11 +4,14 @@
 //
 //  Created by Steve Lou on 10/31/15.
 //  Copyright (c) 2015 Steve. All rights reserved.
+//  代码风格:
+//  函数: AxxxBxxx()
+//  变量: cxxxDxxx
 //
 
 #include "MinheapSort.h"
 
-void MinheapFixup(int a[], int indexNode){
+void MinheapFixUp(int a[], int indexNode){
     int indexParent, keyNode;
     keyNode = a[indexNode];
     indexParent = (indexNode-1)/2;
@@ -28,5 +31,51 @@ void MinheapFixup(int a[], int indexNode){
 
 void MinheapAddNumber(int a[], int indexNode, int data){
     a[indexNode] = data;
-    MinheapFixup(a, indexNode);
+    MinheapFixUp(a, indexNode);
 }
+
+void MinheapFixDown(int a[], int indexNode, int length){
+    int indexSon, keyNode;
+    
+    keyNode = a[indexNode];
+    indexSon = 2*indexNode + 1;
+    while(indexSon < length){
+        if(indexSon+1<length && a[indexSon+1]<a[indexSon]){
+            //找出 a[j]和 a[j+1]中更小的
+            indexSon++;
+        }
+        
+        if(keyNode <= a[indexSon]){
+            //父结点比左右子结点都小,说明排好了
+            break;
+        }
+        
+        a[indexNode] = a[indexSon];
+        indexNode = indexSon;
+        indexSon = indexSon*2 + 1;
+    }
+    a[indexNode] = keyNode;
+}
+
+void MinheapDeleteNumber(int a[], int length){
+    //不用删除 a[length-1],可以留着以后用
+    a[0] = a[length-1];
+    //数组长度len 由length 变成了 length-1
+    MinheapFixDown(a, 0, length-1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
