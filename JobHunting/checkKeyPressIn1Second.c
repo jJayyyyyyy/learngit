fdbe41d260cce9b3b7bbeb77d79e1f369e7b5d99
Steve <ljjdbd123@hotmail.com>
2015-11-05 19:55:01 +0800

    add checkKeyPressIn1Second.c

---
+++ JobHunting/checkKeyPressIn1Second.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/*只有根据之前玩51的经历,大致得到一些想法
+ *
+int checkKeyPressIn1Second(){
+    int count=0;
+    //initTimer();
+    while(1){
+        //key == 0表示键盘按下
+        if(key == 0){
+            delay(10);
+            if(key == 0){
+                count++;
+                //TODO: 开启定时器
+            }
+        }
+        if(到1秒){
+            if(count > 1){
+                return 1;
+            }
+            //TODO: 重置计数器
+        }
+    }
+}
+*/
+
+int main()//主函数
+{
+    return 0;
+}
