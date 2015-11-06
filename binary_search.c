045434848da66c148b549b92bea82e6392f2bc24
Steve <ljjdbd123@hotmail.com>
2015-11-06 22:08:55 +0800

    bug fixed: <--- if(key < a[mid]){     return binarySearch(a, left, mid-1, key);//origin: mid } ---> and replace binary_search() with binarySearch()

--- binary_search.c
+++ binary_search.c
@@ -1,36 +1,35 @@
-// Given a sorted array, and a key_value
-// use binary search to find the index of key_value
+/*Given a sorted(ascengding) array and a key to search its index
+ */
 #include <stdio.h>
-#define	Len_of_Array 6
+#define	LENGTH 6
 
-int binary_search(const int a[], int left, int right, int key){
+
+int binarySearch(const int a[], int left, int right, int key){
     if(left <= right){
         int mid = (left+right)/2;
-        
-        if(key < a[mid])
-            return binary_search(a, left, mid, key);
-        else if(key > a[mid])
-            return binary_search(a, mid+1, right, key);
-        else
+        if(key < a[mid]){
+            return binarySearch(a, left, mid-1, key);
+        }else if(key > a[mid]){
+            return binarySearch(a, mid+1, right, key);
+        }else{
             return mid;
+        }
     }
     
     return -1;
 }
 
 int main(){
-    
-    int array[Len_of_Array] = {1,2,3,4,5,6};
-    int key = 6;
+    int array[LENGTH] = {1,2,3,4,5,6};
+    int key=0, key_index=0;
     
     printf("Please input the key value you want to search: ");
     scanf("%d", &key);
-    printf("key : %d\n", key);
-    printf("The array is: ");
-    for(size_t i=0; i<Len_of_Array; ++i)
+    printf("key : %d\nThe array is: ", key);
+    for(size_t i=0; i<LENGTH; ++i)
         printf("%d ", array[i]);
     
-    int key_index = binary_search(array, 0, Len_of_Array-1, key);
+    key_index = binarySearch(array, 0, LENGTH-1, key);
     if(key_index >= 0)
         printf("\nThe index of key is: %d", key_index);
     else
