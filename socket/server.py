1b18e59988f52436a7204ab0d0d7111a5eca64b9
Steve <ljjdbd123@hotmail.com>
2015-11-10 11:12:25 +0800

    add some comment. write straightly the ip-and-port-of-HOST and bind them together, with more ways of implementation in comment

--- socket/server.py
+++ socket/server.py
@@ -3,11 +3,14 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = socket.gethostname()
-print '\nHost name is %s' % host
-
+host = '192.168.1.100'
+port = 8000
 s.bind(('192.168.1.100', 8000))
-#or we can use the default addr, which is the local addr
+
+#host = socket.gethostname() is the local-ip-address
+#print '\nHost name is %s' % host
+
+#or we can use the default addr, which is also the local-ip-addr
 #	s.bind(('', 8000)) 
 
 while True:
